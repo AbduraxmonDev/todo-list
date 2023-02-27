62dceb6c99d1a79819350519c5d5252f7eb8cb9c
AbduraxmonDev <abduraxmonov000007@gmail.com>
2023-02-27 16:22:54 +0500

    absabff

--- src/components/EntryPage/EntryPage.jsx
+++ src/components/EntryPage/EntryPage.jsx
@@ -5,18 +5,17 @@ function EntryPage() {
 
   window.addEventListener("scroll", function (e) {
     e.preventDefault();
-    // setAddClass(!addClass);
     titleRef.current.classList.add("active");
   });
 
   return (
-    <div className="entry">
-      <section className="entry__section">
+    <section className="entry">
+      <div className="entry__content">
         <h1 className="entry__title" ref={titleRef}>
           todo
         </h1>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 }
 
--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -1,8 +1,53 @@
+import Image from "../../assets/images/image-1.png";
+import Icon from "../../assets/icons/menu-icon.png";
+import CheckedIcon from "../../assets/icons/check.png";
+
 function GetStarted() {
   return (
-    <>
-      <h1>salom</h1>
-    </>
+    <section className="banner">
+      <div className="container">
+        <div className="banner__content">
+          <h1 className="banner__title">
+            <span>t</span>
+            <span>o</span>
+            <span>d</span>
+            <span>o</span>
+          </h1>
+          <p className="banner__text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quis
+            animi corporis porro ad reprehenderit.
+          </p>
+          <a href="/" className="banner__link">
+            Get started
+          </a>
+          <img src={Image} alt="images of 3 girls" className="banner__image" />
+          <div className="banner__card">
+            <div className="card__header">
+              <h5 className="card__title">Download todo app</h5>
+              <img src={Icon} alt="" className="card__menu" />
+            </div>
+            <div className="card__body">
+              <p className="card__text">the first step for better life</p>
+            </div>
+            <div className="card__footer">
+              <div className="card__type">
+                <span className="card__circle"></span>
+                <span className="card__circle"></span>
+              </div>
+              <div className="card__checkbox">
+                <img
+                  src={CheckedIcon}
+                  alt=""
+                  className="checkbox__image"
+                  style={{ width: "12px" }}
+                />
+                <p className="checkbox__text">Done</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
